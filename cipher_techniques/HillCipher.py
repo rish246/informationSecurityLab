45e0238a5d226c19353bc19fa180b345378480e9
rish246 <rishabhkatna2228@gmail.com>
2020-09-24 19:45:20 +0530

    refactored hill cipher code

--- cipher_techniques/HillCipher.py
+++ cipher_techniques/HillCipher.py
@@ -2,116 +2,142 @@ import numpy as np
 import math
 import sympy
 
-plain_text = "attack"
+# plain_text = "attack"
 
-def get_col_vectors(plain_text, key):
-    list_column_vectors = []
-    cur_col_vector = []
+
+class HillCipher:
+
+    def __init__(self, key):
+        self.key = key
+
     
-    len_cur_col_vector = 0
-    
-    for letter in plain_text:
-        cur_col_vector.append(ord(letter) - ord('a'))
-        len_cur_col_vector += 1
-        if(len_cur_col_vector == key.shape[0]):
 
-            col_vector_as_array = np.array(cur_col_vector)
-            # print(col_vector_as_array)
+    ##################################################################################
+    ########## helper functions ######################################################
+    def get_col_vectors(self, plain_text):
+        list_column_vectors = []
+        cur_col_vector = []
+        
+        len_cur_col_vector = 0
+        
+        for letter in plain_text:
+            cur_col_vector.append(ord(letter) - ord('a'))
+            len_cur_col_vector += 1
+            if len_cur_col_vector == self.key.shape[0]:
 
-            list_column_vectors.append(col_vector_as_array)
-            cur_col_vector.clear()
-            len_cur_col_vector = 0
-    return list_column_vectors
+                col_vector_as_array = np.array(cur_col_vector)
+                # print(col_vector_as_array)
 
-def encrypt_col_vectors(list_column_vectors, key):
-    list_encrypted_col_vectors = []
+                list_column_vectors.append(col_vector_as_array)
+                cur_col_vector.clear()
+                len_cur_col_vector = 0
+        return list_column_vectors
 
-    for column_vector in list_column_vectors:
-        encrypted_col_vector = np.dot(key, column_vector) % 26
-        # print(encrypted_col_vector)
-        list_encrypted_col_vectors.append(encrypted_col_vector)
-    
-    return list_encrypted_col_vectors
+    def convert_col_vectors(self, list_column_vectors, key):
+        list_converted_col_vectors = []
 
-
-def get_encrypted_text(list_encrypted_col_vectors):
-    ciphered_text = []
-
-    for col_vector in list_encrypted_col_vectors:
-        first_char, second_char = col_vector
-        ciphered_text.append(chr(math.floor(first_char) + ord('a')))
-        ciphered_text.append(chr(math.floor(second_char) + ord('a')))
+        for column_vector in list_column_vectors:
+            converted_col_vector = np.dot(key, column_vector) % 26
+            # print(encrypted_col_vector)
+            list_converted_col_vectors.append(converted_col_vector)
+        
+        return list_converted_col_vectors
 
 
-    encrypted_text = ''.join(ciphered_text)
+    def get_converted_text(self, list_encrypted_col_vectors):
+        converted_text = []
 
-    return encrypted_text
+        for col_vector in list_encrypted_col_vectors:
+            first_char, second_char = col_vector
+            converted_text.append(chr(math.floor(first_char) + ord('a')))
+            converted_text.append(chr(math.floor(second_char) + ord('a')))
 
-def encrypt(plain_text):
-    list_column_vectors = []
 
+        converted_text = ''.join(converted_text)
+        return converted_text
+
+
+    ##############################################################################
+    ############### main encryption code #########################################
+    def encrypt(self, plain_text):
+        list_column_vectors = []
+
+        # key = np.array([[2, 3], [3, 6]])
+
+        list_column_vectors = self.get_col_vectors(plain_text)
+
+
+        list_encrypted_col_vectors = self.convert_col_vectors(list_column_vectors, self.key)
+
+
+        encrypted_text = self.get_converted_text(list_encrypted_col_vectors)
+
+        return encrypted_text
+
+
+    ###########################################################################
+    ###################### main decryption code ##############################
+    def inverse(self, matrix):
+        determinant = int(np.linalg.det(matrix))
+
+        determinant_inv = int(sympy.invert(determinant, 26))
+
+        matrix_adjascent = np.linalg.inv(matrix) * determinant
+
+        matrix_inv = determinant_inv * matrix_adjascent % 26
+
+        matrix_inv = matrix_inv.astype(int)
+
+        return matrix_inv
+
+
+    def decrypt(self, encrypted_text):
+        key_inverse = self.inverse(self.key)
+
+        # print(key_inverse)
+        list_col_vectors = self.get_col_vectors(encrypted_text)
+
+        list_decrypted_col_vectors = self.convert_col_vectors(list_col_vectors, key_inverse)
+
+        original_text = self.get_converted_text(list_decrypted_col_vectors)
+
+        # print(original_text)
+        return original_text
+    #####################################################################################
+    #####################################################################################
+
+
+
+    # global plain_text
+# key = np.array([[2, 3], [3, 6]])
+# hill_cipher = HillCipher(key)
+
+
+# encrypted_text = hill_cipher.encrypt(plain_text)
+# print(key)
+
+# print(encrypted_text)
+
+# original_text = hill_cipher.decrypt(encrypted_text)
+# print(original_text)
+
+
+# main()
+
+def main():
     key = np.array([[2, 3], [3, 6]])
 
-    list_column_vectors = get_col_vectors(plain_text, key)
+    hill_cipher = HillCipher(key)
 
+    plain_text = "plants"
 
-    list_encrypted_col_vectors = encrypt_col_vectors(list_column_vectors, key)
+    encrypted_text = hill_cipher.encrypt(plain_text)
 
+    print(encrypted_text)
 
-    encrypted_text = get_encrypted_text(list_encrypted_col_vectors)
-
-
-    # print(encrypted_text)
-    return encrypted_text
-
-
-
-def inverse(matrix):
-    determinant = int(np.linalg.det(matrix))
-
-    determinant_inv = int(sympy.invert(determinant, 26))
-
-    matrix_adjascent = np.linalg.inv(matrix) * determinant
-
-    matrix_inv = determinant_inv * matrix_adjascent % 26
-
-    matrix_inv = matrix_inv.astype(int)
-
-    return matrix_inv
-def decrypt_col_vectors(list_col_vectors, key):
-    list_decrypted_col_vectors = []
-
-    # key_inverse = np.linalg.inv(key)
-    key_inverse = inverse(key)
-
-    for column_vector in list_col_vectors:
-
-        decrypted_col_vector = np.dot(key_inverse, column_vector) % 26
-        # print(encrypted_col_vector)
-        list_decrypted_col_vectors.append(decrypted_col_vector)
-    
-    return list_decrypted_col_vectors
-
-def decrypt(plain_text):
-
-    key = np.array([[2, 3], [3, 6]])
-
-    key_inverse = np.linalg.inv(key)
-
-    list_col_vectors = get_col_vectors(plain_text, key)
-
-    list_decrypted_col_vectors = decrypt_col_vectors(list_col_vectors, key)
-
-    # print(list_decrypted_col_vectors)
-    print(key_inverse)
-# ## refactor this code 
-    original_text = get_encrypted_text(list_decrypted_col_vectors)
+    original_text = hill_cipher.decrypt(encrypted_text)
 
     print(original_text)
-    return plain_text
-## use functions 
 
-cypher_text = encrypt(plain_text)
 
-print(cypher_text)
-original_text = decrypt(cypher_text)
+main()
