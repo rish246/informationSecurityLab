1bbad4f358947179cf9535101dd2f7e9b3b7b703
rish246 <rishabhkatna2228@gmail.com>
2020-10-20 11:50:05 +0530

    code refactor(hill cipher and vigenere cipher)

--- cipher_techniques/vignere_cipher.py
+++ cipher_techniques/vignere_cipher.py
@@ -1,19 +1,38 @@
 import numpy as np
 def get_keystream(plain_text, key):
+    """
+    make the key as same size as plaintext by using repetitions
+    eg:
+    plainText = "thisismatch" ==> 11 letters
+    key = "dog"
+    keystream = "dogdogdogdo" ==> 11 letters
+    """
     num_repetitions = int(len(plain_text) / len(key)) + 1
-    key_stream = []
-    for i in range(num_repetitions):
-        key_stream.append(key)
-    key_stream_text = "".join(key_stream)
+
+    key_stream_text = ''.join([key for _ in range(num_repetitions)])
 
     return key_stream_text[0:len(plain_text)]
 
 
 def encrypt(plain_text, key):
+    """
+    Encrypt the plain text using key
+    """
+    # get keystream from the key
     key_stream = get_keystream(plain_text, key)
+
     print(key_stream)
+
+
     vigenere_mat = []
 
+    # convert adjascent pairs into vigenere matrix
+    ## vigenere mat:
+    ## eg: possible letters ==> "a", "b", "c"
+    # vigenere mat --> 
+    # a b c
+    # b c a   --> left shift(1 letter)
+    # c a b   --> left shift(2 letters)
     for row in range(26):
         next_row = []
         for col in range(26):
