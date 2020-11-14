43dd5563c5547c94f7d8c50e035f9aa99281383d
rish246 <rishabhkatna2228@gmail.com>
2020-11-14 14:59:00 +0530

    fixed the bug in euclid algo

--- RSA/RSA.cpp
+++ RSA/RSA.cpp
@@ -7,16 +7,16 @@
 #include <unordered_map>
 #include <queue>
 using namespace std;
-#define PUBLIC_KEY 79
+#define PUBLIC_KEY 65537
 #define BLOCK_SIZE 3
 
-uint64_t plain_text = 688232789878879879;
+int64_t plain_text = 688232789878879879;
 
-uint64_t p = 47, q = 97;
+int64_t p = 65479, q = 23833;
 
-vector<uint64_t> generate_blocks(uint64_t plain_text, int size_block)
+vector<int64_t> generate_blocks(int64_t plain_text, int size_block)
 {
-    vector<uint64_t> output;
+    vector<int64_t> output;
     int dividend = pow(10, size_block);
 
     while (plain_text)
@@ -35,40 +35,40 @@ vector<uint64_t> generate_blocks(uint64_t plain_text, int size_block)
     Implement a recursive algorithm to calculate
 */
 
-uint64_t calc_mod(uint64_t base, uint64_t power, uint64_t mod)
+int64_t calc_mod(int64_t base, int64_t power, int64_t mod)
 {
     if (power == 0)
-        return 1;
+        return (1 % mod);
 
     if (power == 1)
-        return base;
+        return (base % mod);
 
     int mid = (power / 2);
 
     int res = calc_mod(base, mid, mod);
 
-    res = (res * res) % mod;
+    res = ((res % mod) * (res % mod)) % mod;
 
     if (power & 1)
-        res = (res * base) % mod;
+        res = ((res % mod) * (base % mod)) % mod;
 
-    return (res);
+    return (res % mod);
 }
 
-vector<uint64_t> encrypt(vector<uint64_t> plaintext_blocks, uint64_t p, uint64_t q)
+vector<int64_t> encrypt(vector<int64_t> plaintext_blocks, int64_t p, int64_t q)
 {
-    uint64_t n = (p * q);
+    int64_t n = (p * q);
 
     // public key
-    uint64_t e = PUBLIC_KEY;
+    int64_t e = PUBLIC_KEY;
     // blocks generate kar leta
-    vector<uint64_t> output_blocks;
+    vector<int64_t> output_blocks;
 
-    for (uint64_t block : plaintext_blocks)
+    for (int64_t block : plaintext_blocks)
     {
 
         // encrypt block
-        uint64_t encrypted_block = calc_mod(block, e, n);
+        int64_t encrypted_block = calc_mod(block, e, n);
         // convert it into a single number
         output_blocks.push_back(encrypted_block);
     }
@@ -78,9 +78,9 @@ vector<uint64_t> encrypt(vector<uint64_t> plaintext_blocks, uint64_t p, uint64_t
 /* 
     Euclid's algorithm code for finding modulo inverse
 */
-unordered_map<uint64_t, vector<uint64_t>> generate_map(uint64_t A, uint64_t x, uint64_t B, uint64_t y, uint64_t d)
+unordered_map<int64_t, vector<int64_t>> generate_map(int64_t A, int64_t x, int64_t B, int64_t y, int64_t d)
 {
-    unordered_map<uint64_t, vector<uint64_t>> result;
+    unordered_map<int64_t, vector<int64_t>> result;
     while (d > 0)
     {
         result[d] = {A, x, B, y};
@@ -92,24 +92,24 @@ unordered_map<uint64_t, vector<uint64_t>> generate_map(uint64_t A, uint64_t x, u
     return result;
 }
 
-pair<uint64_t, uint64_t> extended_euclid_algo(uint64_t A, uint64_t x, uint64_t B, uint64_t y, uint64_t d)
+pair<int64_t, int64_t> extended_euclid_algo(int64_t A, int64_t x, int64_t B, int64_t y, int64_t d)
 {
 
-    unordered_map<uint64_t, vector<uint64_t>> expansion = generate_map(A, x, B, y, d);
-    unordered_map<uint64_t, uint64_t> consonent;
-    queue<uint64_t> q;
+    unordered_map<int64_t, vector<int64_t>> expansion = generate_map(A, x, B, y, d);
+    unordered_map<int64_t, int64_t> consonent;
+    queue<int64_t> q;
 
     q.push(1);
     consonent[1] = 1;
 
     while (!q.empty())
     {
-        uint64_t cur_d = q.front();
+        int64_t cur_d = q.front();
         q.pop();
 
-        uint64_t first_val = expansion[cur_d][0], first_val_consonent = expansion[cur_d][1];
+        int64_t first_val = expansion[cur_d][0], first_val_consonent = expansion[cur_d][1];
 
-        uint64_t second_val = expansion[cur_d][2], second_val_consonent = expansion[cur_d][3];
+        int64_t second_val = expansion[cur_d][2], second_val_consonent = expansion[cur_d][3];
 
         consonent[first_val] += (consonent[cur_d] * first_val_consonent);
 
@@ -126,68 +126,68 @@ pair<uint64_t, uint64_t> extended_euclid_algo(uint64_t A, uint64_t x, uint64_t B
     return {consonent[A], consonent[B]};
 }
 
-uint64_t mod_inverse_euclid(uint64_t e, uint64_t t)
+int64_t mod_inverse_euclid(int64_t e, int64_t t)
 {
 
     // compute A, x, B, y and d
 
-    uint64_t A = t;
-    uint64_t B = e;
-    uint64_t x = 1;
-    uint64_t y = t / e;
-    uint64_t d = (A * x) - (B * y);
+    int64_t A = t;
+    int64_t B = e;
+    int64_t x = 1;
+    int64_t y = t / e;
+    int64_t d = (A * x) - (B * y);
 
-    pair<uint64_t, uint64_t> result = extended_euclid_algo(A, x, B, y, d);
+    pair<int64_t, int64_t> result = extended_euclid_algo(A, x, B, y, d);
 
-    return result.second;
+    int64_t y_res = result.second;
+
+    return (y_res < 0 ? (y_res + t) : y_res);
 }
 
-vector<uint64_t> decrypt(vector<uint64_t> cipher_blocks, uint64_t p, uint64_t q)
+vector<int64_t> decrypt(vector<int64_t> cipher_blocks, int64_t p, int64_t q)
 {
-    uint64_t n = (p * q);
+    int64_t n = (p * q);
 
-    uint64_t t = (p - 1) * (q - 1);
+    int64_t t = (p - 1) * (q - 1);
 
-    uint64_t e = PUBLIC_KEY;
+    int64_t e = PUBLIC_KEY;
 
     /*
-
     @TODO --> use extended euclid's algo to improve the performance
-
     */
-    uint64_t d = mod_inverse_euclid(e, t);
+    int64_t d = mod_inverse_euclid(e, t);
 
+    d %= t;
     cout << d << endl;
 
-    vector<uint64_t> original_blocks;
-    for (uint64_t cipher_block : cipher_blocks)
+    vector<int64_t> original_blocks;
+    for (int64_t cipher_block : cipher_blocks)
     {
-        uint64_t original_block = calc_mod(cipher_block, d, n);
+        int64_t original_block = calc_mod(cipher_block, d, n);
         original_blocks.push_back(original_block);
     }
     return original_blocks;
 }
 
-void print_vector(vector<uint64_t> v)
+void print_vector(vector<int64_t> v)
 {
-    for (uint64_t e : v)
+    for (int64_t e : v)
         cout << e << " ";
     cout << endl;
 }
 int main()
 {
 
-    vector<uint64_t> plaintext_blocks = generate_blocks(plain_text, BLOCK_SIZE);
+    vector<int64_t> plaintext_blocks = generate_blocks(plain_text, BLOCK_SIZE);
     print_vector(plaintext_blocks);
 
     // encrypt in block of 3
-    vector<uint64_t> cipher_blocks = encrypt(plaintext_blocks, p, q);
-
+    vector<int64_t> cipher_blocks = encrypt(plaintext_blocks, p, q);
     print_vector(cipher_blocks);
 
-    // uint64_t original_text =
-
-    vector<uint64_t> original_blocks = decrypt(cipher_blocks, p, q);
-
+    vector<int64_t> original_blocks = decrypt(cipher_blocks, p, q);
     print_vector(original_blocks);
 }
+
+// inverse modulo cacl : 65537 mod 1560471696
+// 1143906209
--- RSA/power_func_test.cpp
+++ RSA/power_func_test.cpp
@@ -17,31 +17,42 @@ int pow_naive(int base, int power, int mod)
 int pow_better(int base, int power, int mod)
 {
     if (power == 0)
-        return 1;
+        return (1 % mod);
 
     if (power == 1)
-        return base;
+        return (base % mod);
 
     int mid = (power / 2);
 
     int res = pow_better(base, mid, mod);
 
-    res = (res * res) % mod;
+    res = ((res % mod) * (res % mod)) % mod;
 
     if (power & 1)
-        res = (res * base) % mod;
+        res = ((res % mod) * (base % mod)) % mod;
 
-    return (res);
+    return (res % mod);
 }
 
 int main()
 {
-    int base, power, mod;
+    while (true)
+    {
+        int base, power, mod;
 
-    base = 10098;
-    power = 999999999;
-    mod = 875;
-
-    int res = pow_naive(base, power, mod);
-    cout << res << endl;
+        base = (int)rand() % 10000;
+        power = (int)rand() % 13993;
+        mod = (int)rand() % 1345 + 1;
+        int res1 = pow_naive(base, power, mod);
+        int res2 = pow_better(base, power, mod);
+        if (res1 != res2)
+        {
+            cout << res1 << " " << res2 << endl;
+            cout << base << " " << power << " " << mod << endl;
+            break;
+        }
+        cout << "OK" << endl;
+    }
+    // int base = 5358, power = 1, mod = 900;
+    // pow_better(base, power, mod);
 }
